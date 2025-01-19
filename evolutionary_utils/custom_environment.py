63236e33a9efb01ef3eebe4546747e94c02ac7aa
sygkounas <sugkou23@gmail.com>
2025-01-19 17:07:59 +0100

    evolutionary utils update

--- evolutionary_utils/custom_environment.py
+++ evolutionary_utils/custom_environment.py
@@ -1,25 +1,13 @@
 import numpy as np
 
-
 class CustomEnvironment:
     def __init__(self):
-        self.curr_x = 0.0
-        self.curr_y = 0.0
-        self.vx = 0.0
-        self.vy = 0.0
-        self.yaw = 0.0
-        self.pitch = 0.0
-        self.collision = False
-        self.min_pos = 10000.0
-        self.speed = 0
-        self.action_list = np.zeros(4)
-        self.angular_velocity_x = 0.0
-        self.angular_velocity_y = 0.0
-        self.angular_velocity_z = 0.0
-        self.episode_step_counter = 0
-        self.total_step_counter = 0
-        self.distance = 20
+        self.observation = None  # Initialize observation as None
+
+    def update_state(self, observation):
+        self.observation = observation  # Directly store the observation
 
     @property
     def env_state(self):
-        return self.__dict__
+        return {'observation': self.observation}
+    
