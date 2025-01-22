ca1d31407db687ff3bf855b98badf7ddffac1328
sygkounas <sugkou23@gmail.com>
2025-01-22 16:15:55 +0100

    rl logging fix

--- rl_agent/evaluate.py
+++ rl_agent/evaluate.py
@@ -1,17 +1,16 @@
 import json
-
+import logging
 def read_values(file_path):
-    values = []
-    
+    values = [] 
     try:
         with open(file_path, 'r') as file:
             for line in file:
                 if line.strip():  # Ensuring the line is not empty
                     values.append(float(line.strip()))
     except FileNotFoundError:
-        print(f"Error: The file {file_path} does not exist.")
+        logging.error(f"Error: The file {file_path} does not exist.")
     except ValueError as e:
-        print(f"Error parsing float: {e}")
+        logging.error(f"Error parsing float: {e}")
     return values
 
 def calculate_average_around_max(values, window=1000):
@@ -19,17 +18,13 @@ def calculate_average_around_max(values, window=1000):
         return None
 
     max_index = values.index(max(values))
-    print("max index max score",max_index,values[max_index])
     start_index = max(0, max_index - window)
     end_index = min(len(values), max_index + window + 1)
     average = sum(values[start_index:end_index]) / (end_index - start_index)
-    print("average",average)
     return average,max_index
 
 
 def return_score(file_path):
     values = read_values(file_path)
-    print("file path",file_path)
     average,max_index = calculate_average_around_max(values)
-    print("Score at index:",average,max_index)
     return average 
